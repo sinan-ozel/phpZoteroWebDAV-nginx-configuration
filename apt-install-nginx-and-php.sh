a209aa9af2e3e3c6bca7bd2cebd69d06ad188a5a
sinan-ozel <4183117+sinan-ozel@users.noreply.github.com>
2020-12-04 23:11:32 -0500

    Add the fpm module installation to the script.

--- apt-install-nginx-and-php.sh
+++ apt-install-nginx-and-php.sh
@@ -3,4 +3,4 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install nginx-full -y
-sudo apt install php7.0 libapache2-mod_php7.0 apache2-mod_php7.0 php7.0-mysql php7.0-mbstring php7.0-common php7.0-xml php7.0-json php7.0-curl php7.0-zip php7.0-xml -y
+sudo apt install php7.0 php7.0-fpm libapache2-mod_php7.0 apache2-mod_php7.0 php7.0-mysql php7.0-mbstring php7.0-common php7.0-xml php7.0-json php7.0-curl php7.0-zip php7.0-xml -y
